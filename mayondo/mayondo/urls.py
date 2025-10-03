e3b8dab5faa85dfd7d9ecfaf28edb8be68630d2c
Keith James Seruyange <keithseruyange@gmail.com>
2025-10-03 19:04:23 +0300

    Created the inital dashboards and landing page

--- mayondo/mayondo/urls.py
+++ mayondo/mayondo/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from users.views import dashboard_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='landing'),
+    path('dashboard/', dashboard_router, name='dashboard_router'),
+    
     path('users/', include('users.urls')),
     path('manager/', include('manager.urls')),
     path('products/', include('products.urls')),
